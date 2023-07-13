42fa644555c2b51fb6780a1a5ae738c292a7de7e
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-07-13 10:35:43 +0000

    Factories

--- test-app/database/seeders/DatabaseSeeder.php
+++ test-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Listing::create([
+            'title' => 'Laravel Senior Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'test@example.com',
+            'website' => 'www.xample.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
+        ]);
+        Listing::create([
+            'title' => 'FullStack Developer',
+            'tags' => 'PHP, laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'test@example.com',
+            'website' => 'www.xample.com',
+            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
+        ]);
     }
 }
