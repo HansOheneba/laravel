28961f0a36b5f7ff4ffd82d2bea69a2c8ce02ce7
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-07-14 16:45:24 +0000

    theme

--- test-app/database/seeders/DatabaseSeeder.php
+++ test-app/database/seeders/DatabaseSeeder.php
@@ -16,23 +16,25 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
-        Listing::create([
-            'title' => 'Laravel Senior Developer',
-            'tags' => 'laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'test@example.com',
-            'website' => 'www.xample.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
-        ]);
-        Listing::create([
-            'title' => 'FullStack Developer',
-            'tags' => 'PHP, laravel, javascript',
-            'company' => 'Acme Corp',
-            'location' => 'Boston, MA',
-            'email' => 'test@example.com',
-            'website' => 'www.xample.com',
-            'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
-        ]);
+        Listing::factory(6)->create();
+
+        // Listing::create([
+        //     'title' => 'Laravel Senior Developer',
+        //     'tags' => 'laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'test@example.com',
+        //     'website' => 'www.xample.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
+        // ]);
+        // Listing::create([
+        //     'title' => 'FullStack Developer',
+        //     'tags' => 'PHP, laravel, javascript',
+        //     'company' => 'Acme Corp',
+        //     'location' => 'Boston, MA',
+        //     'email' => 'test@example.com',
+        //     'website' => 'www.xample.com',
+        //     'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eos consequatur perferendis corporis quasi repudiandae necessitatibus minus, minima sapiente. Officiis a debitis qui et, dolorum asperiores quam culpa quos repellat tempora totam aliquid voluptatem? Beatae saepe distinctio a tenetur natus, illum nulla blanditiis odit dolore magnam molestias, tempora consectetur neque.',
+        // ]);
     }
 }
