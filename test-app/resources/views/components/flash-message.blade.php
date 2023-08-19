608f57c165266278cdbb9c74a525f2b2f6bc9614
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-08-19 15:26:24 +0000

    AlpineJS

--- test-app/resources/views/components/flash-message.blade.php
+++ test-app/resources/views/components/flash-message.blade.php
@@ -1,5 +1,12 @@
 @if(session()->has('message'))
 <div x-data="{show: true}" x-init="setTimeout(()=> show = false, 2500)" 
-    x-show="show" class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-green-300 text-white px-48 py-3"><p>
+    x-show="show"
+        x-transition:enter="transition ease-out duration-300"
+        x-transition:enter-start="opacity-0 scale-90"
+        x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-300"
+        x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-90" 
+        class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48 py-3"><p>
     {{session('message')}}</p></div>
 @endif
