b03a2e7144eb920dc72369c700b05351882a989c
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-08-20 07:35:31 +0000

    Delete listing

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -29,6 +29,12 @@ Route::post('/listings', [ListingController::class, 'store']);
 //Store Edit Form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+//Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
+//Delete Listing
+Route::Delete('/listings/{listing}', [ListingController::class, 'destroy']);
+
 //Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
