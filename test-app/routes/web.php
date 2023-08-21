e5e037facf72513d6bae193c9b7ae37d2d73fcd4
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-08-21 14:46:17 +0000

    layout update

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 use Symfony\Component\HttpFoundation\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 /*
@@ -37,4 +38,10 @@ Route::Delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 //Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']); 
+
+// Create New User
+Route::post('/users', [UserController::class, 'store']);
 
